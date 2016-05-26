4ff5ba2b1bc48006fb4c800392465c2dc933465b
hbklenin <hbk.lenin5@gmail.com>
2016-05-26 16:04:53 -0500

    Operaciones Bàsicas

---
+++ JAVA/src/Operaciones/Operaciones.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operaciones;
+
+/**
+ *
+ * @author Luis
+ */
+public class Operaciones {
+    public int Sumar(int x, int y){
+       int suma=x+y;
+        return suma;
+    }
+    public int Resta(int x, int y){
+       int resta=x-y;
+        return resta;
+    }
+    public int Multiplicacion(int x, int y){
+       int mul=x*y;
+        return mul;
+    }
+    public int Division(int x, int y){
+       int div=x/y;
+        return div;
+    }
+}
